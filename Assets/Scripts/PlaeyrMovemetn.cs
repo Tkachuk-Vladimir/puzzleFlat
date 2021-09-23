5ac6d9f0b6a8b5e91c9faafc81ba585d74dc405d
vovatk <tck.vova@ya.ru>
2021-09-23 15:33:15 +0300

    add files

---
+++ Assets/Scripts/PlaeyrMovemetn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class PlaeyrMovemetn : MonoBehaviour
+{
+    [SerializeField] KeyCode keyOne;
+    [SerializeField] KeyCode keyTwo;
+    [SerializeField] Vector3 moveDirection;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(keyOne))
+        {
+            GetComponent<Rigidbody>().velocity -= moveDirection;
+        }
+        if(Input.GetKey(keyTwo))
+        {
+            GetComponent<Rigidbody>().velocity += moveDirection;
+        }
+        if(Input.GetKey(KeyCode.R)) //if we press the key R, the level will reload
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/buttonActivator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonActivator : MonoBehaviour
+{
+    public GameObject firstGroup;
+    public Material normal;
+    public Material transparent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cube")|| other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().material = transparent;
+            firstGroup.GetComponent<Renderer>().material = transparent;
+            firstGroup.GetComponent<Collider>().isTrigger = true;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cube") || other.CompareTag("Player"))
+        {
+            GetComponent<Renderer>().material = normal; 
+            firstGroup.GetComponent<Renderer>().material = normal; ;
+            firstGroup.GetComponent<Collider>().isTrigger = false;
+
+        }
+    }
+}
